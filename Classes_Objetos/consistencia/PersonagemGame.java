696b1ebb0328afaf07ee6ec9d318bd5822904c3c
Thiago Rosendo <thiago_953@hotmail.com>
2025-07-29 15:00:16 -0300

    consistencia

---
+++ Classes_Objetos/consistencia/PersonagemGame.java
@@ -0,0 +1,54 @@
+public class PersonagemGame {
+    private String nome;
+    private int saudeAtual;
+    private String status;
+
+    public PersonagemGame(int saudeAtual, String nome) {
+        this.setSaudeAtual(saudeAtual);
+        this.nome = nome;
+    }
+
+    public void setNome (String nome) {
+        if (nome != null && !nome.isEmpty()){
+            this.nome = nome;
+        }       
+    }
+
+    public void setSaudeAtual (int saudeAtual) {
+        this.saudeAtual = saudeAtual;
+        if (this.saudeAtual > 0) {
+            this.status = "vivo";
+        } else {
+            this.status = "morto";
+        }
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public int getSaudeAtual() {
+        return this.saudeAtual;
+    }
+
+    public void tomarDano (int quantidadeDeDano) {
+        if (this.getSaudeAtual() - quantidadeDeDano < 0 ){
+            this.setSaudeAtual(0);
+        } else {
+            this.setSaudeAtual(this.getSaudeAtual() - quantidadeDeDano);
+        }
+    }
+
+    public void receberCura (int quantidadeDeCura) {
+        if (this.getSaudeAtual() + quantidadeDeCura > 100 ){
+            this.setSaudeAtual(100);
+        } else {
+            this.setSaudeAtual(this.getSaudeAtual() + quantidadeDeCura);
+        }
+    }
+
+    public String getStatus() {
+        return this.status;
+    }
+
+}
