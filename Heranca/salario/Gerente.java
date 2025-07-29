6094d6e7cf8d7f2afb069ffb4c33e7d7cbe768e6
Thiago Rosendo <thiago_953@hotmail.com>
2025-07-29 17:39:33 -0300

    salario

--- Heranca/salario/Gerente.java
+++ Heranca/salario/Gerente.java
@@ -1,24 +1,22 @@
 public class Gerente extends Empregado {
 
-    private double salarioFixo;
-
     public Gerente(double salarioFixo) {
         super(salarioFixo);
     }
 
     @Override
     public double calcularSalarioTotal(Departamento departamento) {
-        return this.salarioFixo + this.calcularBonus(departamento);
+        return super.getSalarioFixo() + this.calcularBonus(departamento);
     }
 
     @Override
     public double calcularBonus(Departamento departamento) {
         if (departamento.alcancouMeta()) {
-            double diferanca = departamento.getValorAtingidoMeta() - departamento.getValorAtingidoMeta();
+            double diferanca = departamento.getValorAtingidoMeta() - departamento.getValorMeta();
             if (diferanca < 0){
                 diferanca = 0;
             }
-            return (this.salarioFixo * 0.2) + (diferanca * 0.01);
+            return (super.getSalarioFixo() * 0.2) + (diferanca * 0.01);
         }
         return 0;
     }
