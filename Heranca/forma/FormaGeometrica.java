f24af06334972fdb821507d28ac46d0efef6c82c
Thiago Rosendo <thiago_953@hotmail.com>
2025-07-29 17:50:44 -0300

    salario

--- Heranca/forma/FormaGeometrica.java
+++ Heranca/forma/FormaGeometrica.java
@@ -1,5 +1,5 @@
 public class FormaGeometrica {
     public double area() throws UnsupportedOperationException {
-        throw new UnsupportedOperationException("Funcao areao nao implementada");
+        throw new UnsupportedOperationException("Funcao area nao implementada");
     }
 }
