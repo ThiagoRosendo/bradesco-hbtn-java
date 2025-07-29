4585f6cd25e8229a46e77fdac7f951253d006561
Thiago Rosendo <thiago_953@hotmail.com>
2025-07-29 12:49:41 -0300

    metodos

---
+++ Classes_Objetos/metodos/PersonagemGame.java
@@ -0,0 +1,35 @@
+public class PersonagemGame {
+    private String nome;
+    private int saudeAtual;
+
+    public void setNome (String nome) {
+        this.nome = nome;
+    }
+
+    public void setSaudeAtual (int saudeAtual) {
+        this.saudeAtual = saudeAtual;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public int getSaudeAtual() {
+        return this.saudeAtual;
+    }
+
+    public void tomarDano (int quantidadeDeDano) {
+        this.saudeAtual -= quantidadeDeDano;
+        if (this.saudeAtual < 0 ){
+            this.saudeAtual = 0;
+        }
+    }
+
+    public void recebeCura (int quantidadeDeCura) {
+        this.saudeAtual -= quantidadeDeCura;
+        if (this.saudeAtual > 100 ){
+            this.saudeAtual = 100;
+        }
+    }
+    
+}
