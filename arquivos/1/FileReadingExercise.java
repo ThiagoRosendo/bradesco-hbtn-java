e8b460f77255fd392040258bc977e3389f430529
Thiago Rosendo <thiago_953@hotmail.com>
2025-08-14 15:07:00 +0000

    1. Leitura de Arquivos

---
+++ arquivos/1/FileReadingExercise.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileReadingExercise {
+    public static void main(String[] args) {
+        String fileName = "exemplo.txt"; // Nome do arquivo fixo para leitura
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String linha;
+
+            while ((linha = br.readLine()) != null) {
+                System.out.println(linha);
+            }
+
+        } catch (IOException e) {
+            System.out.println("Erro ao ler o arquivo: " + e.getMessage());
+        }      
+    }
+}
