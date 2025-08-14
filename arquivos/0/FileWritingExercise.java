88c513c1fb37b66c7f2834cc2f01d203aa23a751
Thiago Rosendo <thiago_953@hotmail.com>
2025-08-14 14:55:22 +0000

    0. Escrevendo arquivos

---
+++ arquivos/0/FileWritingExercise.java
@@ -0,0 +1,33 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class FileWritingExercise {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Solicita o nome do arquivo
+        System.out.print("Digite o nome do arquivo (com extensão .txt): ");
+        String fileName = scanner.nextLine();
+
+        try (FileWriter arquivo = new FileWriter(fileName, true);
+                PrintWriter linha = new PrintWriter(arquivo)) {
+            System.out.println("Digite o conteúdo do arquivo:");
+            while (true) {
+                String text = scanner.nextLine();
+                if (text.equalsIgnoreCase("sair")) {
+                    break;
+                }
+                linha.println(text);
+            }
+            System.out.println("Arquivo criado om sucesso");
+
+        } catch (IOException e) {
+            System.out.println("Erro ao gravar arquivo: " + e.getMessage());
+        }
+
+        scanner.close();
+    }
+}
