7f48590dd6f8ef3e97ae90eeae0f741cab405169
Thiago Rosendo <thiago_953@hotmail.com>
2025-08-15 15:28:58 +0000

    2. Lista TODO

--- List/todo/ListaTodo.java
+++ List/todo/ListaTodo.java
@@ -1,5 +1,59 @@
-package List.todo;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ListaTodo {
-    
+    private List<Tarefa> tarefas;
+
+    public ListaTodo() {
+        this.tarefas = new ArrayList<Tarefa>();
+    }
+
+    public void adicionarTarefa(Tarefa tarefa) {
+        if (!tarefas.stream().anyMatch(p -> p.getIdentificador() == tarefa.getIdentificador()) ) {
+            tarefas.add(tarefa);
+        } else {
+            throw new IllegalArgumentException(
+                String.format("Tarefa com identificador %d ja existente na lista", tarefa.getIdentificador()));
+        }
+    }
+
+    public boolean marcarTarefaFeita(int identificador) {
+        for (int i = 0; i < tarefas.size(); i++) {
+            if (tarefas.get(i).getIdentificador() == identificador) {
+                tarefas.get(i).setEstahfeita(true);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean desfazerTarefa(int identificador) {
+        for (int i = 0; i < tarefas.size(); i++) {
+            if (tarefas.get(i).getIdentificador() == identificador) {
+                tarefas.get(i).setEstahfeita(false);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void desfazerTodas() {
+        for (int i = 0; i < tarefas.size(); i++) {
+            tarefas.get(i).setEstahfeita(false);
+        }
+    }
+
+    public void fazerTodas() {
+        for (int i = 0; i < tarefas.size(); i++) {
+            tarefas.get(i).setEstahfeita(true);
+        }
+    }
+
+    public void listarTarefas() {
+        for (Tarefa tarefa : tarefas) {
+            System.out.printf("[%s]  Id: %d - Descricao: %s\n", tarefa.getEstahFeita(), tarefa.getIdentificador(),
+                    tarefa.getDescricao());
+        }
+    }
+
 }
--- List/todo/Tarefa.java
+++ List/todo/Tarefa.java
@@ -1,5 +1,37 @@
-package List.todo;
-
 public class Tarefa {
-    
+    private String descricao;
+    private boolean estahfeita;
+    private int identificador;
+
+    public Tarefa(String descricao, int identificador){
+        this.descricao = descricao;
+        this.identificador = identificador;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public int getIdentificador() {
+        return identificador;
+    }
+
+    public String getEstahFeita() {
+        if (estahfeita) {
+            return "X";
+        }
+        return " ";
+    }    
+
+    public void setEstahfeita(boolean estahfeita) {
+        this.estahfeita = estahfeita;
+    }
+
+    public void modificarDescircao(String descricao){
+        if (descricao.equals(" ") || descricao == null) {
+            throw new IllegalArgumentException("Descricao de tarefa invalida");
+        } else {
+            this.descricao = descricao;
+        }
+    }
 }
