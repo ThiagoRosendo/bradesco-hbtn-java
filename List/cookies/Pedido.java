f3735262c5e8f78ea5acc1ab55e7926ff66391bb
Thiago Rosendo <thiago_953@hotmail.com>
2025-08-15 14:42:05 +0000

    1. Cookies

--- List/cookies/Pedido.java
+++ List/cookies/Pedido.java
@@ -1,5 +1,35 @@
-package List.cookies;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Pedido {
+    private List<PedidoCookie> cookies;
     
+    public Pedido(){
+        this.cookies = new ArrayList<PedidoCookie>();
+    }
+
+    public void adicionarPedidoCookie(PedidoCookie pedidoCookie){
+        cookies.add(pedidoCookie);
+    }
+
+    public int obterTotalCaixas(){
+        int totalCaixas = 0;
+
+        for (PedidoCookie pedidoCookie : cookies) {
+            totalCaixas += pedidoCookie.getQuantidadeCaixas();
+        }
+
+        return totalCaixas;
+    }
+
+    public int removerSabor(String sabor){
+        int quantidadeRemovidos = 0;
+        for (int i = 0; i < cookies.size(); i++) {
+            if (cookies.get(i).getSabor() == sabor) {
+                quantidadeRemovidos += cookies.get(i).getQuantidadeCaixas();
+                cookies.remove(i);
+            }
+        }
+        return quantidadeRemovidos;
+    }
 }
--- List/cookies/PedidoCookie.java
+++ List/cookies/PedidoCookie.java
@@ -1,5 +1,18 @@
-package List.cookies;
-
 public class PedidoCookie {
+    private String sabor;
+    private int quantidadeCaixas;
+    
+    public PedidoCookie(String sabor, int quantidadeCaixas){
+        this.sabor = sabor;
+        this.quantidadeCaixas = quantidadeCaixas;
+    }
+
+    public String getSabor() {
+        return sabor;
+    }
+
+    public int getQuantidadeCaixas() {
+        return quantidadeCaixas;
+    }
     
 }
