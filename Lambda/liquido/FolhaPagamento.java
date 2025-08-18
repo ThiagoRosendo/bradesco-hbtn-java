ffccaaab8426d6ab952ca6276a241f0deeffe8bf
Thiago Rosendo <thiago_953@hotmail.com>
2025-08-18 18:46:17 +0000

    1. Salario Funcionario

--- Lambda/liquido/FolhaPagamento.java
+++ Lambda/liquido/FolhaPagamento.java
@@ -1,6 +1,6 @@
 public class FolhaPagamento {
     
-    public static double calcularLiquido(Funcionario funcionario, CalculadoraDeSalario calculadoraDeSalario){
+    public static double calcularLiquido(Funcionario funcionario, CalculadorDeSalario calculadoraDeSalario){
         return calculadoraDeSalario.calcular(funcionario.getSalario());
     }
 }
